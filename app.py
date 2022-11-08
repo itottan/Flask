e312eb4b77519c588b79ca54fcaed47367d5ab1a
itottan <itottan@gmail.com>
2022-11-09 01:05:30 +0900

    ファイルのアップロードについて

--- app.py
+++ app.py
@@ -2,7 +2,22 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask, render_template ,request, redirect, url_for, abort
+from werkzeug.utils import secure_filename
+import pykakasi
+import os
+
 app = Flask(__name__)
+
+class Kakasi:
+    k = pykakasi.kakasi()
+    k.setMode('H', 'a')
+    k.setMode('K', 'a')
+    k.setMode('J', 'a')
+
+    conv =k.getConverter()
+    @classmethod
+    def jp_to_ascii(cls, jp): # cls[class], jp[変換したい文字列]
+        return cls.conv.do(jp)
 
 @app.route('/')
 def index():
@@ -88,6 +103,28 @@ def page_not_fount(error):
     # コード404の場合、NotFoundページへ遷移し、404コードを戻す
     return render_template('not_found.html'), 404
 
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'GET':
+        return render_template('upload.html')
+    if request.method == 'POST':
+        # inputタグname値がuploadImgの対象を取得
+        file = request.files['uploadImg']
+        # ファイル名を変更 file.name : uploadImg file.filename : IMG_2165.JPG
+        save_fileNm = secure_filename(Kakasi.jp_to_ascii(file.filename))
+        # サーバーへ保存
+        file.save(os.path.join('./static/img', save_fileNm))
+        # When build a URL with url_for() in Flask
+        # you should pass the endpoint of the view function, the default value of endpoint is the name of the view function, not the URL rule
+        # 
+        # url_forの第一引数はView関数の名前
+        # url_for第二引数以後はurlに渡す。渡す引数が既にurlの中に定義された場合はそのままpathの形でurlに渡す、定義されない場合はDictとしてurlに渡す
+        return redirect(url_for('uploaded', filename = save_fileNm))
+
+@app.route('/uploaded_file/<string:filename>')
+# 引数はroutePathに設定した引数に一致する必要がある
+def uploaded(filename):
+    return render_template('uploaded_file.html', fileNm= filename)
 
 if __name__ == '__main__': #TODO if文にした原因
     app.run(debug=True) #開発モード用、動的コンパイル
